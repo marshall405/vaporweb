248156b203ef0584853d0f333ffae997ed939f8d
rhinehardtc <rhinehardtc@gmail.com>
2020-06-13 15:44:56 -0500

    User login works until page refresh

--- vapor-backend-api/app/controllers/users_controller.rb
+++ vapor-backend-api/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def user
+        p params
+        user = User.find_by(username: params['userName'])
+        if user
+            render json: :user
+        else
+            render json: {message: 'User Not Found, Coward.'}
+        end
+    end
 end
