29fce87d7c60fbb9916feb1926ca339e3656f272
rhinehardtc <rhinehardtc@gmail.com>
2020-06-13 15:43:00 -0500

    User can login until page refresh

--- vapor-frontend/src/index.js
+++ vapor-frontend/src/index.js
@@ -1,8 +1,9 @@
-
+const userURL = 'http://localhost:3000/users';
 
 
 //  LOGIN 
-document.getElementById('login').addEventListener('click', showLoginForm)
+const login = document.getElementById('login');
+login.addEventListener('click', showLoginForm);
 
 function showLoginForm(e) {
     document.getElementById('login-form').style.display = "flex"
@@ -15,9 +16,31 @@ document.getElementById('login-form').addEventListener('submit', handleLogin);
 function handleLogin(e) {
     e.preventDefault()
 
+    const userName = e.target.username.value;
+
+    fetch(userURL, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({userName})
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (!json.message){
+            const welcomeUser = document.createElement('h2');
+            welcomeUser.innerText = `Welcome to the future past,\n ${userName}.`;
+            document.querySelector('header').appendChild(welcomeUser);
+            login.style.display = 'none';
+            document.getElementById('login-form').style.display = "none"
+        } else {
+            alert(json.message)
+        }
+    });
+
     // fetch username details and render stuff
-    document.querySelector('h1').style.color = 'red';
-    document.getElementById('login-form').style.display = "none"
+    
     // or alert invalid username? 
 
     e.target.username.value = "";
